2742135751b74e6439a5d2dcb2b68097b00cc3b6
Lucas Victor <lvitors@hotmail.com>
2026-06-28 15:27:30 -0300

    feat: adding response status to exceptions

--- src/main/java/br/com/tech/challenger/api_restaurante/application/exception/LoginAlreadyExistsException.java
+++ src/main/java/br/com/tech/challenger/api_restaurante/application/exception/LoginAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package br.com.tech.challenger.api_restaurante.application.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class LoginAlreadyExistsException extends RuntimeException {
     public LoginAlreadyExistsException(String message) {
         super(message);
--- src/main/java/br/com/tech/challenger/api_restaurante/application/exception/RestaurantNotFoundException.java
+++ src/main/java/br/com/tech/challenger/api_restaurante/application/exception/RestaurantNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.tech.challenger.api_restaurante.application.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class RestaurantNotFoundException extends RuntimeException {
     public RestaurantNotFoundException(String message) {
         super(message);
